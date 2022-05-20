c27977a9d31085888010f7745c36bea562683a6b
yunimm <yuu9916@gmail.com>
2022-05-20 18:15:49 +0800

    清除目前用不到的檔案

--- src/views/TouristSpot.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import TouristListPanel from '@/components/TouristListPanel.vue'
-</script>
-<template>
-  <h1 class="h-full w-full bg-blue-600"></h1>
-  <TouristListPanel />
-</template>
