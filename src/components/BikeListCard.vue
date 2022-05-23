1a29ee5d37c7c91803d5038a2f528f20b0aef058
yunimm <yuu9916@gmail.com>
2022-05-23 12:37:35 +0800

    fix: 修正車站租借狀態css設定

--- src/components/BikeListCard.vue
+++ src/components/BikeListCard.vue
@@ -10,7 +10,9 @@ const bikes = useBikeStore()
   await bikes.getData()
 })()
 
-const { newData, lat, lng, isLoading } = storeToRefs(useBikeStore())
+const { newData, lat, lng, isLoading, cardLat, cardLng } = storeToRefs(
+  useBikeStore()
+)
 
 watch([lat, lng], ([newLat, newLng]) => {
   {
@@ -18,27 +20,48 @@ watch([lat, lng], ([newLat, newLng]) => {
     bikes.getData()
   }
 })
+
+function onClick(id) {
+  newData.value.forEach((item) => {
+    if (item.id == id) {
+      cardLat.value = item.lat
+      cardLng.value = item.lng
+    }
+  })
+}
 </script>
 <template>
-  <div v-for="item in bikes.searchData" class="mb-9" :id="item.uid">
-    <div class="mt-4 flex flex-nowrap items-center justify-between">
+  <div
+    v-for="item in bikes.searchData"
+    class="my-4 cursor-pointer rounded p-4 shadow-lg outline-primary-400 duration-100 hover:outline"
+    :id="item.uid"
+    @click.stop.prevent="onClick(item.id)"
+  >
+    <div class="flex flex-nowrap items-center justify-between">
       <div class="flex items-center">
         <span
-          class="mr-2 inline-block w-40 truncate text-[0.94rem] font-bold tracking-wide text-primary-500 xl:text-xl"
+          class="mr-2 inline-block w-40 truncate text-[0.94rem] font-bold tracking-wide text-primary-500 xl:w-64 xl:text-xl"
           >{{ item.name.Zh_tw }}</span
         >
+        <!-- 手機版狀態標示 -->
         <span
           v-show="item.rent && item.return > 0"
           class="defaultState xl:hidden"
           >可借可還</span
         >
-        <span v-show="item.return === 0" class="singleService xl:hidden"
+        <span
+          v-show="item.rent === 0 && item.return > 0"
+          class="singleService xl:hidden"
           >只可借車</span
         >
-        <span v-show="item.rent === 0" class="singleService xl:hidden"
+        <span
+          v-show="item.return === 0 && item.rent > 0"
+          class="singleService xl:hidden"
           >只可停車</span
         >
-        <span v-show="item.rent && item.return === 0" class="fixState xl:hidden"
+        <span
+          v-show="item.rent === 0 && item.return === 0"
+          class="fixState xl:hidden"
           >站點施工中</span
         >
       </div>
@@ -51,7 +74,7 @@ watch([lat, lng], ([newLat, newLng]) => {
       <BorrowInfo
         :class="{
           isZero: item.rent === 0,
-          isLimit: item.rent < 5,
+          isLimit: item.rent <= 5 && item.rent > 0,
           isEnough: item.rent > 5,
         }"
       >
@@ -61,9 +84,9 @@ watch([lat, lng], ([newLat, newLng]) => {
       </BorrowInfo>
       <ParkingInfo
         :class="{
-          isZero: item.rent === 0,
-          isLimit: item.rent < 5,
-          isEnough: item.rent > 5,
+          isZero: item.return === 0,
+          isLimit: item.return <= 5 && item.return > 0,
+          isEnough: item.return > 5,
         }"
       >
         <template v-slot:parking-number>
@@ -71,14 +94,19 @@ watch([lat, lng], ([newLat, newLng]) => {
         </template>
       </ParkingInfo>
     </ul>
+    <!-- 電腦版狀態標示 -->
     <div class="mt-3 hidden xl:block">
       <div class="flex items-center justify-between">
         <span v-show="item.rent && item.return > 0" class="defaultState"
           >可借可還</span
         >
-        <span v-show="item.return === 0" class="singleService">只可借車</span>
-        <span v-show="item.rent === 0" class="singleService">只可停車</span>
-        <span v-show="item.rent && item.return === 0" class="fixState"
+        <span v-show="item.rent === 0 && item.return > 0" class="singleService"
+          >只可借車</span
+        >
+        <span v-show="item.return === 0 && item.rent > 0" class="singleService"
+          >只可停車</span
+        >
+        <span v-show="item.rent === 0 && item.return === 0" class="fixState"
           >站點施工中</span
         >
         <div class="text-grey-500">
--- src/components/ParkingInfo.vue
+++ src/components/ParkingInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="xl:flex xl:flex-col xl:justify-center">
+  <li class="xl:flex xl:flex-col xl:justify-center xl:pb-1">
     <div>
       <font-awesome-icon class="mr-1 text-xs xl:text-base" icon="parking" />
       <span class="mr-3 text-[0.94rem]">可停車</span>
